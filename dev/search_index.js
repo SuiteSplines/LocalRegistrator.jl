3d5c95d262b4867c9e48a93d5350ef380aea4af2
Documenter.jl <documenter@juliadocs.github.io>
2025-06-14 07:36:30 +0000

    build based on 44fcad1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LocalRegistrator","page":"Home","title":"LocalRegistrator","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LocalRegistrator.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
